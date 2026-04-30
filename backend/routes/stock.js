bf76ea5ee7610b1af0483cc296ac10301ee0d957
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-04-30 07:36:49 -0300

    Update stock.js

--- backend/routes/stock.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ mensagem: 'Rota de estoque funcionando' });
-});
-
-module.exports = router;
