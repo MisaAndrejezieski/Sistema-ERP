0405422bf8a00d9c73388afef8d8e3e924585df6
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-04-30 07:35:34 -0300

    Update clients.js

--- backend/routes/clients.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ mensagem: 'Rota de clientes funcionando' });
-});
-
-module.exports = router;
