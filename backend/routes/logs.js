526dcbea1fe8fa9063f9dfb8aa4dd9d2b5b1339c
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-04-30 07:37:20 -0300

    Update logs.js

--- backend/routes/logs.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ mensagem: 'Rota de logs funcionando' });
-});
-
-module.exports = router;
