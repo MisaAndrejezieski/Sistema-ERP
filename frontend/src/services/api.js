df6e53bc807fdd3fbf5c9dec6793b6282bd6e610
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-05-01 09:59:45 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sistema-erp-api.onrender.com/api', // SUA URL DO RENDER
+  baseURL: 'https://sistema-erp-vuyg.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
